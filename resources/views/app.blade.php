a3d33e8b8a2b9b9614e6953086b97944bec937d2
Thomas Freeborough <thomas@hotsnapper.com>
2017-09-06 09:53:56 +0100

    [Typo] Update title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title') - Firecat</title>
+    <title>@yield('title') | beat.fund</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
