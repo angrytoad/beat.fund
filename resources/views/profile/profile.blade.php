13944a8901fc2f55d213c257ef29b7022ae54579
Thomas Freeborough <thomas@hotsnapper.com>
2018-03-17 15:54:35 +0000

    [Profile] Add more profile panels

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -8,6 +8,30 @@
     <div class="row">
         <div class="col-md-3">
             @include('layouts.menus.internal_menu')
+
+            @if(Auth::user()->profile->getCompletionPercentage() < 100)
+                <div class="panel panel-success">
+                    <div class="panel-heading">Profile Progress</div>
+                    <div class="panel-body">
+                        <p>
+                            Your profile is <strong>{{ Auth::user()->profile->getCompletionPercentage() }}%</strong> complete, finish creating
+                            your profile so you can start creating a storefront.
+                        </p>
+                    </div>
+                </div>
+            @endif
+
+            @if(Auth::user()->profile->getCompletionPercentage() === 100)
+                <div class="panel panel-info">
+                    <div class="panel-heading">Create a Store</div>
+                    <div class="panel-body">
+                        <p>
+                            Thanks for having a complete profile! You may now go and <a href={{ route('storefront.create') }}>Create a storefront.</a>
+                        </p>
+                    </div>
+                </div>
+            @endif
+
         </div>
         <div class="col-md-9">
             <div class="panel panel-default">
