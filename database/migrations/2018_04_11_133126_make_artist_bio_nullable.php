887403c0c41b0352b3c32a695cb44e45f08fd52e
Thomas Freeborough <thomas@hotsnapper.com>
2018-04-11 14:32:35 +0100

    [Artist Bio] Allow it to be nullable

---
+++ database/migrations/2018_04_11_133126_make_artist_bio_nullable.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeArtistBioNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn('artist_bio');
+        });
+
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->mediumText('artist_bio')->nullable()->after('artist_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn('artist_bio');
+        });
+
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->text('artist_bio')->nullable()->after('artist_name');
+        });
+    }
+}
