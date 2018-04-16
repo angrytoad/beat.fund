31795f5c82ddf00ecc16a36663559b9f604bbf80
Alex Salam <vornat2@gmail.com>
2018-04-16 22:20:08 +0100

    updates

--- resources/views/account/admin/admin_panel.blade.php
+++ resources/views/account/admin/admin_panel.blade.php
@@ -5,5 +5,14 @@
 @section ('content')
     <div class="container" id="admin-panel">
         <h1>Admin Panel</h1>
+        <div class="col col-lg-12">
+            <div class="col col-lg-8">
+                <h2>User control panel</h2>
+                <p>The user control panel, list all current active users and be able to inspect their stores/products and reset them if required.</p>
+            </div>
+            <div class="col col-lg-4">
+                <div style="text-align: center" class="btn btn-primary">Users Panel</div>
+            </div>
+        </div>
     </div>
 @endsection
