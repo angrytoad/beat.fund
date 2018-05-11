47bfa50384ffe725d865e899f5a2fc3caf40855c
Thomas Freeborough <thomas@hotsnapper.com>
2018-05-11 11:03:44 +0100

    [New Frontpage]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app_basic')
 
 @section('title', 'Welcome')
 
@@ -9,51 +9,16 @@
             <div id="welcome-container-child">
                 <div id="welcome-container-child-container">
                     <h1 id="title">Beat Fund</h1>
-                    <h3 id="subtitle">Supporting independent artists</h3>
-                </div>
-            </div>
-        </div>
-        <div id="welcome-background">
-            <div id="welcome-background-items">
-                @foreach($background_products as $key => $product)
-                    <div class="welcome-background-item">
-                        <img src="{{ $product->downsizedImage() }}" />
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </div>
-    <div id="welcome-content">
-        <div id="double_action">
-            <a href="{{ route('storefront') }}">
-                <div id="double_action_left" class="double_action_item" style="background: url('/images/misc/cassettes.jpg') center center / cover;">
-                    <div>
-                        <h2>Take me to the store</h2>
-                    </div>
-                </div>
-            </a>
-            <a href="{{ route('storefront.random') }}">
-                <div id="double_action_left" class="double_action_item" style="background: url('/images/misc/live_gig.jpg') center center / cover;">
-                    <div>
-                        <h2>Help me discover something new.</h2>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div id="preamble">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xs-12">
-                        <h1 class="title">Proudly supporting independent artists.</h1>
-                    </div>
-                    <div class="col-xs-12" id="preamble-text">
-                        <p>
-                            We like music, but what we like more than music is making sure that Artists who create it are paid fairly.
-                        </p>
-                        <p>
-                            Beat Fund wants to make a difference, our vision is to offer all the tools an Artist needs 100% free, with a revenue
-                            sharing policy that's fair.
-                        </p>
+                    <h3 id="subtitle">Supporting independent artists for FREE, FOREVER</h3>
+                    <div id="store-sampler">
+                        @foreach(\App\Helpers\Helper::getArtistStoresSampler() as $store)
+                            <a href="{{ route('artist.store', $store->slug) }}">
+                                <div class="artist">
+                                    <p class="artist-name">{{ $store->user->profile->artist_name }}</p>
+                                    <img src="{{ $store->downsizedAvatar() }}" />
+                                </div>
+                            </a>
+                        @endforeach
                     </div>
                 </div>
             </div>
